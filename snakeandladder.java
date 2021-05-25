6560df17ae0fc9239417b82871a59fd395bfa0cc
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-05-25 17:14:15 +0530

    adding options to check the move

--- snakeandladder.java
+++ snakeandladder.java
@@ -7,6 +7,35 @@ int position = 0;
 
 int diceroll = (1 + (int)(Math.random () * 6 ));
 
+int option = (int)((Math.random () * 10) % 3 ) ;
+
+	System.out.println( + option);
+
+switch (option)
+{
+	case 1:
+	//ladder
+	position = position + diceroll;
+	System.out.println("current position is : " + position);
+	break;
+
+	case 2:
+	//snake
+	if (position > diceroll)
+
+	{
+	position = position - diceroll ;
+	System.out.println("current position is : " + position);
+	}
+	break;
+	 
+	default:
+	//no play
+	position = position;
+	System.out.println("current position is : " + position);
+
+}
+
 System.out.println(+ diceroll );
 
 }
